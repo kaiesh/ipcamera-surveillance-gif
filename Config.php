fa2c1b8949f22fdbbc8b2be41355c1eed4465b68
Kaiesh Vohra <kaiesh@kaiesh.com>
2019-01-26 13:42:25 +0800

    Add PHP files for IP camera management

---
+++ Config.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is intended to hold variables that are specific to your Configuration
+ **/
+
+class Config{
+
+  //********************************
+  //***** General operations *******
+  //********************************
+
+  //Set your timezone so that datestamps on images are as expected!
+  public static $timezone = "Asia/Singapore";
+
+  //Number of loops to run each time the file is executed, -1 for a never ending loop
+  public static $LOOP_COUNT = 19;
+
+  //Time to sleep between loops (in seconds)
+  public static $SLEEP_DURATION = 3;
+
+  //Minimum number of frames required before a GIF archive file is created
+  public static $MIN_FRAMES_TO_GIF = 1200;
+
+  //Maximum number of frames to put into one GIF. You may need to play around with this value as it will depend on your image sizes, available memory to PHP, and your PC processing+memory capacity
+  public static $MAX_FRAMES_TO_GIF = 1500;
+
+  //Set the time for the animated GIF archive to be created
+  //this will check to see if the requisite number of files exists during this hour, and if so compile the GIF accordingly
+  //Use '*' if creation time does not matter, or specify the hour in 24h format (e.g. 23 = 11pm)
+  public static $CONVERT_TIME = "*"; //convert the JPGs in GIF archives as soon as the min number of frames is met
+
+  //*********************************************
+  //***** File system and storage options *******
+  //*********************************************
+
+  //Full path and filename for where the latest frame should be stored
+  public static $CURRENT_FRAME_FULLFILEPATH = "./camfile.jpg";
+
+  //Where should JPG frames be stored before they are used for GIFs?
+  public static $JPEG_LOCATION = "./archive/";
+
+  //Where should GIF animaion archives be stored?
+  public static $GIF_TARGET = "./gifs/";
+
+  //*************************************************
+  //***** Details for each surveillance frame *******
+  //*************************************************
+
+  //Most IP cameras allow you to access the current frame via a specific URL, simply create an array of all of your camera frame URLs, and the expected source dimensions
+  public static $CAMERA_IMAGE_URL_ARR = array(
+    array("url"=>'http://camera.url:port/endpoint', "x"=>640, "y"=>480),
+    array("url"=>'http://camera.url:port/endpoint', "x"=>640, "y"=>480),
+    array("url"=>'http://camera.url:port/endpoint', "x"=>640, "y"=>480),
+    array("url"=>'http://camera.url:port/endpoint', "x"=>640, "y"=>480),
+    array("url"=>'http://camera.url:port/endpoint', "x"=>640, "y"=>480),
+    array("url"=>'http://camera.url:port/endpoint', "x"=>640, "y"=>480)
+  );
+
+  //How much veritcal space should be left for your frame label text (which is approx 10px high and has a 5px margin at the top)
+  public static $FRAME_LABEL_Y = 20;
+
+  //What should the final dimensions of your surveillence graphic be (including frame label space)?
+  public static $FRAME_DIMENSIONS = array("x"=>640, "y"=>340);
+
+  //How many rows and columns of images should exist on the surveillence frame?
+  public static $FRAME_LAYOUT = array("cols"=>3, "rows"=>2);
+
+
+  //********************************************
+  //***** GIF archive animation settings *******
+  //********************************************
+
+  //How many milliseconds should each frame stay on screen for?
+  public static $ANIM_DURATION = 33;
+
+
+}
---
+++ cameras.cli.php
@@ -0,0 +1,80 @@
+<?php
+require_once("Config.php");
+require_once("GifCreator.php");
+
+date_default_timezone_set(Config::$timezone);
+
+//create the thumbnail base image
+$x = 0;
+while(Config::$LOOP_COUNT < 0 || $x < Config::$LOOP_COUNT){
+	echo "\nRun ".$x.": ";
+	$img_disp = imagecreatetruecolor(Config::$FRAME_DIMENSIONS["x"], Config::$FRAME_DIMENSIONS["y"]);
+	for ($i=0; $i < count(Config::$CAMERA_IMAGE_URL_ARR); $i++){
+		echo $i." ";
+		$im = imagecreatefromjpeg(Config::$CAMERA_IMAGE_URL_ARR[$i]["url"]);
+		imagecopyresized(
+			$img_disp,
+			$im,
+			($i%Config::$FRAME_LAYOUT["cols"]) * floor(Config::$FRAME_DIMENSIONS["x"]/Config::$FRAME_LAYOUT["cols"]),
+			($i%Config::$FRAME_LAYOUT["rows"]) * floor((Config::$FRAME_DIMENSIONS["y"]-Config::$FRAME_LABEL_Y)/Config::$FRAME_LAYOUT["rows"]),
+			0, 0,
+			floor(Config::$FRAME_DIMENSIONS["x"]/Config::$FRAME_LAYOUT["cols"]),
+			floor(Config::$FRAME_DIMENSIONS["y"]/Config::$FRAME_LAYOUT["rows"]),
+			Config::$CAMERA_IMAGE_URL_ARR[$i]["x"], Config::$CAMERA_IMAGE_URL_ARR[$i]["y"]);
+	}
+	$txt_col = imagecolorallocate($img_disp, 255,255,255);
+	imagestring($img_disp, 2, 10,Config::$FRAME_DIMENSIONS["y"]-Config::$FRAME_LABEL_Y+5, "Camera Array ".date("G:i:s d/m/Y"),$txt_col);
+	imagejpeg($img_disp, Config::$CURRENT_FRAME_FULLFILEPATH,50);
+	imagejpeg($img_disp, Config::$JPEG_LOCATION.date("Ymd-His").".jpg",75);
+	imageDestroy($img_disp);
+	imageDestroy($im);
+	echo " * ";
+	$x++;
+	sleep(Config::$SLEEP_DURATION);
+}
+
+//With the latest JPG frames made, now check if these need to be archived into a GIF
+if (Config::$CONVERT_TIME=="*" || date("H") === Config::$CONVERT_TIME){
+	$files = scandir(Config::$JPEG_LOCATION);
+	$tgt_file = Config::$GIF_TARGET.date("Y-m-d-H-i")."gif";
+	$gif_frame_src = array();
+	$gif_frame_time = array();
+	$filecount = 0;
+
+	$total_files = count($files);
+	$gif_file_count = min(array(Config::$MAX_FRAMES_TO_GIF, $total_files));
+
+	if ($total_files > Config::$MIN_FRAMES_TO_GIF){
+		echo "Starting GIF creation process... ";
+		for($i=0; $i < $gif_file_count ; $i++){
+			$filename = $files[$i];
+			if ( is_file(Config::$JPEG_LOCATION.$filename) ){
+				$filecount++;
+				$gif_frame_src[] = Config::$JPEG_LOCATION.$filename;
+				$gif_frame_time[] = Config::$ANIM_DURATION;
+			}
+		}
+		if (count($gif_frame_src) > 2){
+
+			$tgt_file = Config::$GIF_TARGET.str_replace(array(Config::$JPEG_LOCATION,".jpg"),
+			"", $gif_frame_src[0])."-to-".str_replace(array(Config::$JPEG_LOCATION,".jpg"),"",$gif_frame_src[count($gif_frame_src)-1]).".gif";
+			echo count($gif_frame_src)." frames found, starting on them so they get stored in ".$tgt_file."... ";
+			$gc = new GifCreator();
+			$gc->create($gif_frame_src, $gif_frame_time, 1);
+			$gifBinary = $gc->getGif();
+			file_put_contents($tgt_file, $gifBinary);
+			echo "OK. GIF anim made and stored in ".$tgt_file."\n";
+			echo "Cleaning up archive files... ";
+			foreach ($gif_frame_src as $frame){
+				unlink($frame);
+			}
+			unset($gc); unset($gifBinary); unset($gif_frame_time); unset($files); unset($filecount);
+			echo "OK. Deleted ".count($gif_frame_src)." files\n";
+			unset($gif_frame_src);
+		}else{
+			echo "ERR: **** NO GIF ANIM MADE ****\n";
+		}
+	}else{
+		echo "\n**** NOT ENOUGH FILES TO MAKE A GIF ****\n";
+	}
+}
